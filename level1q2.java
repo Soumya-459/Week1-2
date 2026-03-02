28e402f5c0d00785a891d54798eb3771b77f1588
SG-mon156 <sg6625@srmist.edu.in>
2026-03-02 09:13:14 +0530

    Added level1q2 implementation

---
+++ level1q2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class level1q2{
+   public static void main(String[] args){
+     int maths=94;
+     int physics=95;
+     int chem=96;
+     double avg=(maths+chem+physics)/3;
+     System.out.println("Sam's average mark in PCM is " + avg);
+}}
